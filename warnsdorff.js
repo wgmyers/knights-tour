f358e35c1176707a2f518258423e78f1ebf50c1e
Wayne Myers <wgmyers@gmail.com>
2019-10-20 02:40:58 +0100

    Add utility function to find knights moves

--- warnsdorff.js
+++ warnsdorff.js
@@ -6,6 +6,28 @@ var warnsdorff = function warnsdorff() {
     board: []
   };
 
+  // get_knights_moves
+  // Take x and y position on board
+  // Return array of squares a knights move away from x,y
+  function get_knights_moves(x,y) {
+    var res = [];
+      // Iterate over a 5x5 square centered on x,y, skipping non knights moves
+      for (var i = -2; i < 3; i++) {
+        if (i == 0) { continue; } // Skip non knights moves
+        for (var j = -2; j < 3; j++) {
+          if ((j == 0) || (i == j) || (i == -j)) { continue; } // Skip non knights moves
+          var cx = x + i;
+          var cy = y + j;
+          if((cx > 0) && (cx < data.width) && (cy > 0) && (cy < data.height)) {
+            res.push([cx, cy]);
+          }
+         }
+      }
+
+    return res;
+  }
+
+  // Initialise data for the Warnsdoff algorithm
   function init() {
     // Create board array, 8x8 for now
     for (var i = 0; i < data.height; i++) {
